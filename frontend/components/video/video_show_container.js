1bf1692035b3dd148b39125562acee644602c7b6
Qixiang <qixiang100@gmail.com>
2020-02-18 18:39:30 -0500

    refactored video show container to take in new state

--- frontend/components/video/video_show_container.js
+++ frontend/components/video/video_show_container.js
@@ -4,18 +4,25 @@ import VideoShow from './video_show';
 import { getVideo, getAllVideos } from '../../actions/video_actions'
 
 const mapStateToProps = (state, ownProps) => {
-  const videoId = ownProps.match.params.videoId;
-  const video = state.entities.videos[videoId];
-  const author = ( video && video.user ? video.user : null);
-  const recommended = Object.assign({}, state.entities.videos);
-  delete recommended[videoId];
+
+  let video = null;
+  let author = null;
+  let users = null;
+  let recommended = null;
+
+  if (state.entities.videos && state.entities.videos.video){
+    video = (state.entities.videos.video ? state.entities.videos.video.video : null);
+    author = (state.entities.videos.video ? state.entities.videos.video.user : null);
+    users = (state.entities.videos.video ? state.entities.videos.video.users : null);
+    recommended = Object.values(state.entities.videos.video.videos);
+  }
 
   return {
     video: video,
     author: author,
     recommended: recommended,
-    currentUser: state.session.id
-    // users: state.entities.users,
+    currentUser: state.session.id,
+    users: users
   };
 };
 
