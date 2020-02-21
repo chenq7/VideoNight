184e090c698333fb34b4eda31eabcbd1edce1733
Qixiang <qixiang100@gmail.com>
2020-02-21 13:32:18 -0500

    Fixed a bug

--- frontend/reducers/errors/video_errors_reducer.js
+++ frontend/reducers/errors/video_errors_reducer.js
@@ -6,7 +6,7 @@ const videoErrorsReducer = (state = [], action) => {
 
   switch (action.type) {
     case RECEIVE_VIDEO_ERRORS:
-      return action.errors;
+      return action.errors || [];
     case CLEAR_ERRORS:
       return []
     default:
