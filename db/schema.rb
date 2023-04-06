fa12462affd781c001125266e2a3d2232ee45caf
Greg Chen <greg@Gregs-MBP.lan>
2023-04-05 23:29:15 -0400

    update migrations and db seed

--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_052023) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_04_06_014149) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,9 +30,16 @@ ActiveRecord::Schema.define(version: 2020_03_31_052023) do
     t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
+    t.string "checksum"
     t.datetime "created_at", null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
@@ -83,4 +89,5 @@ ActiveRecord::Schema.define(version: 2020_03_31_052023) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -40,135 +40,136 @@ i_like_learning = User.create({username: "I like Learning", email: "l@gmail.com"
 
 # Movie Trailer Seeds
 vid1 = Video.create({title:"Marvel Studios Avengers: Infinity War Official Trailer", description: "There was an idea... Avengers: Infinity War. In theaters April 27.", view_count: 721, author_id: marvel.id })
-thumbnail1 = open('https://video-night-seeds.s3.amazonaws.com/avengers.jpg')
+thumbnail1 = URI.open('https://video-night-seeds.s3.amazonaws.com/avengers.jpg')
 vid1.thumbnail.attach(io: thumbnail1, filename: "avengers.jpg")
-file1 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/avengers.mp4")
+file1 = URI.open("https://video-night-seeds.s3.amazonaws.com/avengers.mp4")
 vid1.video.attach(io: file1, filename: 'avengers.mp4')
 vid1.save
 
+
 vid2 = Video.create({title:"STRANGER THINGS 4 Trailer Teaser (2020)", description: "STRANGER THINGS 4 Trailer Teaser (2020) Season 4, S04 Hopper © 2020 - Netflix", view_count: 481, author_id: one_media.id })
-thumbnail2 = open('https://video-night-seeds.s3.amazonaws.com/stranger_things.jpg')
+thumbnail2 = URI.open('https://video-night-seeds.s3.amazonaws.com/stranger_things.jpg')
 vid2.thumbnail.attach(io: thumbnail2, filename: "stranger_things.jpg")
-file2 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/stranger_things.mp4")
+file2 = URI.open("https://video-night-seeds.s3.amazonaws.com/stranger_things.mp4")
 vid2.video.attach(io: file2, filename: 'stranger_things.mp4')
 vid2.save
 
 vid3 = Video.create({title:"The Turning - Official Trailer", description: "Keeping the lights on won't keep you safe. Watch the trailer for #TheTurningMovie, in theaters January 24.", view_count: 221, author_id: universal.id })
-thumbnail3 = open('https://video-night-seeds.s3.amazonaws.com/turning.jpg')
+thumbnail3 = URI.open('https://video-night-seeds.s3.amazonaws.com/turning.jpg')
 vid3.thumbnail.attach(io: thumbnail3, filename: "turning.jpg")
-file3 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/turning.mp4")
+file3 = URI.open("https://video-night-seeds.s3.amazonaws.com/turning.mp4")
 vid3.video.attach(io: file3, filename: 'turning.mp4')
 vid3.save
 
 vid4 = Video.create({title:"Jurassic World: Fallen Kingdom - Official Trailer [HD]", description: "Jurassic World: Fallen Kingdom. In Theaters June 22, 2018 ", view_count: 567, author_id: universal.id })
-thumbnail4 = open('https://video-night-seeds.s3.amazonaws.com/jurassic_park.jpg')
+thumbnail4 = URI.open('https://video-night-seeds.s3.amazonaws.com/jurassic_park.jpg')
 vid4.thumbnail.attach(io: thumbnail4, filename: "jurassic_park.jpg")
-file4 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/jurassic_park.mp4")
+file4 = URI.open("https://video-night-seeds.s3.amazonaws.com/jurassic_park.mp4")
 vid4.video.attach(io: file4, filename: 'jurassic_park.mp4')
 vid4.save
 
-vid5 = Video.create({title:"The Lion King Official Trailer", description: "Disney's The Lion King opens in theaters July 19, 2019. Watch the new trailer now. ", view_count: 137, author_id: disney.id })
-thumbnail5 = open('https://video-night-seeds.s3.amazonaws.com/lion_king.jpg')
+vid5 = Video.create({title:"The Lion King Official Trailer", description: "Disney's The Lion King URI.opens in theaters July 19, 2019. Watch the new trailer now. ", view_count: 137, author_id: disney.id })
+thumbnail5 = URI.open('https://video-night-seeds.s3.amazonaws.com/lion_king.jpg')
 vid5.thumbnail.attach(io: thumbnail5, filename: "lion_king.jpg")
-file5 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/lion_king.mp4")
+file5 = URI.open("https://video-night-seeds.s3.amazonaws.com/lion_king.mp4")
 vid5.video.attach(io: file5, filename: 'lion_king.mp4')
 vid5.save
 
 # Pokemon moment seeds
 vid6 = Video.create({title:"Pokemon funny scene - Ghastly scares Jesse", description: "Short video clip from Pokemon series", view_count: 221, author_id: pokemon_fan.id })
-thumbnail6 = open('https://video-night-seeds.s3.amazonaws.com/poke1.jpg')
+thumbnail6 = URI.open('https://video-night-seeds.s3.amazonaws.com/poke1.jpg')
 vid6.thumbnail.attach(io: thumbnail6, filename: "poke1.jpg")
-file6 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/poke1.mp4")
+file6 = URI.open("https://video-night-seeds.s3.amazonaws.com/poke1.mp4")
 vid6.video.attach(io: file6, filename: 'poke1.mp4')
 vid6.save
 
 vid7 = Video.create({title:"Evil pikachu", description: "So evil", view_count: 601, author_id: pokemon_fan.id })
-thumbnail7 = open('https://video-night-seeds.s3.amazonaws.com/poke2.jpg')
+thumbnail7 = URI.open('https://video-night-seeds.s3.amazonaws.com/poke2.jpg')
 vid7.thumbnail.attach(io: thumbnail7, filename: "poke2.jpg")
-file7 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/poke2.mp4")
+file7 = URI.open("https://video-night-seeds.s3.amazonaws.com/poke2.mp4")
 vid7.video.attach(io: file7, filename: 'poke2.mp4')
 vid7.save
 
 vid8 = Video.create({title:"Alolan Marowak Dances! Funny Moments! Pokemon Sun And Moon Episode 34", description: "Alolan Marowak Funny Dance", view_count: 91, author_id: pokemon_fan.id })
-thumbnail8 = open('https://video-night-seeds.s3.amazonaws.com/poke3.jpg')
+thumbnail8 = URI.open('https://video-night-seeds.s3.amazonaws.com/poke3.jpg')
 vid8.thumbnail.attach(io: thumbnail8, filename: "poke3.jpg")
-file8 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/poke3.mp4")
+file8 = URI.open("https://video-night-seeds.s3.amazonaws.com/poke3.mp4")
 vid8.video.attach(io: file8, filename: 'poke3.mp4')
 vid8.save
 
 vid9 = Video.create({title:"Meltan too op", description: "op op", view_count: 55, author_id: pokemon_fan.id })
-thumbnail9 = open('https://video-night-seeds.s3.amazonaws.com/poke4.jpg')
+thumbnail9 = URI.open('https://video-night-seeds.s3.amazonaws.com/poke4.jpg')
 vid9.thumbnail.attach(io: thumbnail9, filename: "poke4.jpg")
-file9 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/poke4.mp4")
+file9 = URI.open("https://video-night-seeds.s3.amazonaws.com/poke4.mp4")
 vid9.video.attach(io: file9, filename: 'poke4.mp4')
 vid9.save
 
 vid10 = Video.create({title:"Cute Mew! - Pokémon Movie 22 Mewtwo Strikes Back Evolution [HD]", description: "Look how cute he is", view_count: 355, author_id: nayan.id })
-thumbnail10 = open('https://video-night-seeds.s3.amazonaws.com/poke5.jpg')
+thumbnail10 = URI.open('https://video-night-seeds.s3.amazonaws.com/poke5.jpg')
 vid10.thumbnail.attach(io: thumbnail10, filename: "poke5.jpg")
-file10 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/poke5.mp4")
+file10 = URI.open("https://video-night-seeds.s3.amazonaws.com/poke5.mp4")
 vid10.video.attach(io: file10, filename: 'poke5.mp4')
 vid10.save
 
 
 # Learning seeds
 vid11 = Video.create({title:"Min/Max Rescaler Coding Quiz - Intro to Machine Learning", description: "This video is part of an online course, Intro to Machine Learning.", view_count: 115, author_id: udacity.id })
-thumbnail11 = open('https://video-night-seeds.s3.amazonaws.com/code1.jpg')
+thumbnail11 = URI.open('https://video-night-seeds.s3.amazonaws.com/code1.jpg')
 vid11.thumbnail.attach(io: thumbnail11, filename: "code1.jpg")
-file11 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/code1.mp4")
+file11 = URI.open("https://video-night-seeds.s3.amazonaws.com/code1.mp4")
 vid11.video.attach(io: file11, filename: 'code1.mp4')
 vid11.save
 
 vid12 = Video.create({title:"Maximal Variance - Intro to Machine Learning", description: "This video is part of an online course, Intro to Machine Learning.", view_count: 95, author_id: udacity.id })
-thumbnail12 = open('https://video-night-seeds.s3.amazonaws.com/code2.jpg')
+thumbnail12 = URI.open('https://video-night-seeds.s3.amazonaws.com/code2.jpg')
 vid12.thumbnail.attach(io: thumbnail12, filename: "code2.jpg")
-file12 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/code2.mp4")
+file12 = URI.open("https://video-night-seeds.s3.amazonaws.com/code2.mp4")
 vid12.video.attach(io: file12, filename: 'code2.mp4')
 vid12.save
 
 vid13 = Video.create({title:"A New Feature - Intro to Machine Learning", description: "This video is part of an online course, Intro to Machine Learning.", view_count: 146, author_id: udacity.id })
-thumbnail13 = open('https://video-night-seeds.s3.amazonaws.com/code3.png')
+thumbnail13 = URI.open('https://video-night-seeds.s3.amazonaws.com/code3.png')
 vid13.thumbnail.attach(io: thumbnail13, filename: "code3.png")
-file13 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/code3.mp4")
+file13 = URI.open("https://video-night-seeds.s3.amazonaws.com/code3.mp4")
 vid13.video.attach(io: file13, filename: 'code3.mp4')
 vid13.save
 
 vid14 = Video.create({title:"[sol: age is continuous] - Intro to Machine Learning", description: "This video is part of an online course, Intro to Machine Learning.", view_count: 152, author_id: udacity.id })
-thumbnail14 = open('https://video-night-seeds.s3.amazonaws.com/code4.jpg')
+thumbnail14 = URI.open('https://video-night-seeds.s3.amazonaws.com/code4.jpg')
 vid14.thumbnail.attach(io: thumbnail14, filename: "code4.jpg")
-file14 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/code4.mp4")
+file14 = URI.open("https://video-night-seeds.s3.amazonaws.com/code4.mp4")
 vid14.video.attach(io: file14, filename: 'code4.mp4')
 vid14.save
 
 vid15 = Video.create({title:"Practice With Margins - Intro to Machine Learning", description: "This video is part of an online course, Intro to Machine Learning.", view_count: 72, author_id: udacity.id })
-thumbnail15 = open('https://video-night-seeds.s3.amazonaws.com/code5.jpg')
+thumbnail15 = URI.open('https://video-night-seeds.s3.amazonaws.com/code5.jpg')
 vid15.thumbnail.attach(io: thumbnail15, filename: "code5.jpg")
-file15 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/code5.mp4")
+file15 = URI.open("https://video-night-seeds.s3.amazonaws.com/code5.mp4")
 vid15.video.attach(io: file15, filename: 'code5.mp4')
 vid15.save
 
 
 # Created by demo user
 vid16 = Video.create({title:"3D scenes of nature", description: "Cool 3d animation of nature, enjoy!", view_count: 521, author_id: demo_user.id })
-thumbnail16 = open('https://video-night-seeds.s3.amazonaws.com/nature.jpg')
+thumbnail16 = URI.open('https://video-night-seeds.s3.amazonaws.com/nature.jpg')
 vid16.thumbnail.attach(io: thumbnail16, filename: "nature.jpg")
-file16 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/nature.mp4")
+file16 = URI.open("https://video-night-seeds.s3.amazonaws.com/nature.mp4")
 vid16.video.attach(io: file16, filename: 'nature.mp4')
 vid16.save
 
 vid17 = Video.create({title:"Hummingbird with flowers - 30 second animation", description: "3d animation of a humming bird in nature. Be sure to like, comment, and subscribe!", view_count: 822, author_id: demo_user.id })
-thumbnail17 = open('https://video-night-seeds.s3.amazonaws.com/humming_bird.jpg')
+thumbnail17 = URI.open('https://video-night-seeds.s3.amazonaws.com/humming_bird.jpg')
 vid17.thumbnail.attach(io: thumbnail17, filename: "humming_bird.jpg")
-file17 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/humming_bird.mp4")
+file17 = URI.open("https://video-night-seeds.s3.amazonaws.com/humming_bird.mp4")
 vid17.video.attach(io: file17, filename: "humming_bird.mp4")
 vid17.save
 
 # Created by meme user
 
 vid18 = Video.create({title:"Funny minions video || 30 sec minions video", description: "", view_count: 438, author_id: meme_user.id })
-thumbnail18 = open('https://video-night-seeds.s3.amazonaws.com/minion_fighting.jpg')
+thumbnail18 = URI.open('https://video-night-seeds.s3.amazonaws.com/minion_fighting.jpg')
 vid18.thumbnail.attach(io: thumbnail18, filename: "minion_fighting.jpg")
-file18 = EzDownload.open("https://video-night-seeds.s3.amazonaws.com/minion_fighting.mp4")
+file18 = URI.open("https://video-night-seeds.s3.amazonaws.com/minion_fighting.mp4")
 vid18.video.attach(io: file18, filename: 'minion_fighting.mp4')
 vid18.save
 
